3690a11c38e7ba768ac458bae573f042c79444ab
Prasanna_emmadi <prasanna.177@gmail.com>
2022-08-19 11:29:32 +0300

    Updating schema of itemPictures

--- models/product.js
+++ models/product.js
@@ -5,7 +5,14 @@ const ProductSchema = new mongoose.Schema({
   itemName: String,
   itemPrice: Number,
   itemDescription: String,
-  itemPictures: { pic1: String, pic2: String },
+  itemPictures: {
+    description: "Pictures for the products",
+    type: "array",
+    items: {
+      description: "List of pictures",
+      type: "string",
+    },
+  },
 });
 //              first String is Name of the collection in database
 const ProductModel = mongoose.model("Product", ProductSchema);
