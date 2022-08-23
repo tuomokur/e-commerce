8db03ddf2124754eaba82afa6f620639d10436a8
Prasanna_emmadi <prasanna.177@gmail.com>
2022-08-23 14:49:22 +0300

    Fixing the broken build

--- easybuy_front/src/routes/Routes.js
+++ easybuy_front/src/routes/Routes.js
@@ -1,32 +1,43 @@
-import styles from "./App.module.css";
+import styles from "../App.module.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { GridItem } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
-import Home from "./routes/Home";
-import Login from "./routes/Login";
-import Register from "./routes/Register";
-import Category from "./routes/Category";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import Category from "./Category";
 
-const Routes = () => {
+const AppRoutes = () => {
   return (
     <ChakraProvider>
       <Router>
-        <GridItem pl="2" bg="white.300" area={"header"}>
-          <nav>
-            <li className={styles.title}>EasyBuy</li>
+        <Grid
+          templateAreas={`"header"
+                  
+            `}
+          gridTemplateRows={"250px "}
+          gridTemplateColumns={"1200px"}
+          h="120px"
+          gap="1"
+          color="blackAlpha.700"
+          fontWeight="bold"
+        >
+          <GridItem pl="2" bg="white.300" area={"header"}>
+            <nav>
+              <li className={styles.title}>EasyBuy</li>
 
-            <li className={styles.home}>
-              <Link to="/">Home</Link>
-            </li>
-            <li className={styles.home}>
-              <Link to="/Login">Login</Link>
-            </li>
-            <li className={styles.home}>
-              <Link to="/Register">Register</Link>
-            </li>
-          </nav>
-        </GridItem>
-
+              <li className={styles.home}>
+                <Link to="/">Home</Link>
+              </li>
+              <li className={styles.home}>
+                <Link to="/Login">Login</Link>
+              </li>
+              <li className={styles.home}>
+                <Link to="/Register">Register</Link>
+              </li>
+            </nav>
+          </GridItem>
+        </Grid>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
@@ -37,4 +48,4 @@ const Routes = () => {
     </ChakraProvider>
   );
 };
-export default Routes;
+export default AppRoutes;
