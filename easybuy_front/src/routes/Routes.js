2d0aa59c59665482abea1756b0de64203e6d1a87
Prasanna Emmadi <prasanna.177@gmail.com>
2022-08-24 13:09:03 +0000

    adding navigation bar

--- easybuy_front/src/routes/Routes.js
+++ easybuy_front/src/routes/Routes.js
@@ -1,43 +1,16 @@
-import styles from "../App.module.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
 import Category from "./Category";
+import HeaderBar from "./components/HeaderBar";
 
 const AppRoutes = () => {
   return (
     <ChakraProvider>
       <Router>
-        <Grid
-          templateAreas={`"header"
-                  
-            `}
-          gridTemplateRows={"250px "}
-          gridTemplateColumns={"1200px"}
-          h="120px"
-          gap="1"
-          color="blackAlpha.700"
-          fontWeight="bold"
-        >
-          <GridItem pl="2" bg="white.300" area={"header"}>
-            <nav>
-              <li className={styles.title}>EasyBuy</li>
-
-              <li className={styles.home}>
-                <Link to="/">Home</Link>
-              </li>
-              <li className={styles.home}>
-                <Link to="/Login">Login</Link>
-              </li>
-              <li className={styles.home}>
-                <Link to="/Register">Register</Link>
-              </li>
-            </nav>
-          </GridItem>
-        </Grid>
+        <HeaderBar />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
