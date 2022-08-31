40a42ca663aae19a5d56ce9d7b1bafad386cbdde
suman.nepali <hlw.zigri@gmail.com>
2022-08-31 12:27:05 +0300

    footer component

--- easybuy_front/src/index.js
+++ easybuy_front/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import AuthProvider from "./contexts/authContext";
 
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <AuthProvider>
+      
         <App />
-      </AuthProvider>
+      
     </ChakraProvider>
 
   </React.StrictMode >
