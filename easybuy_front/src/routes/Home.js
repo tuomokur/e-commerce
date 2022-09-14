3c9b8f653a920e0047a8fdc49ce0208be6c8fc8f
Prasanna_emmadi <prasanna.177@gmail.com>
2022-09-14 12:30:38 +0300

    added searchIcon

--- easybuy_front/src/routes/Home.js
+++ easybuy_front/src/routes/Home.js
@@ -1,6 +1,6 @@
 import LandingImage from "./components/LandingImage.js";
 import RecentItems from "./components/RecentItems.js";
-import SearchBar from "./components/SearchBar.js";
+import SearchBar from "./components/Searchbar.js";
 import { Flex, Spacer } from "@chakra-ui/react";
 
 const Home = () => {
