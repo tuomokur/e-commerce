c3f02b7d2c937e23a99045a90f5648fb2e2b393d
Suman Nepali <hlw.zigri@gmail.com>
2022-09-26 11:29:24 +0000

    recent item component

--- easybuy_front/src/routes/Home.js
+++ easybuy_front/src/routes/Home.js
@@ -1,18 +1,17 @@
 import LandingImage from "./components/LandingImage.js";
 import RecentItems from "./components/RecentItems.js";
 import SearchBar from "./components/Searchbar.js";
-import { Flex, Spacer } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <div className="main-container">
-      <Flex>
-        <LandingImage />
-        <Spacer />
-        <SearchBar />
-        {/* <RecentItems /> */}
-      </Flex>
-    </div>
+      <Box>
+        <Stack isInline>
+          <LandingImage />
+          <SearchBar />
+        </Stack>
+        <RecentItems />
+      </Box>
   );
 };
 export default Home;
